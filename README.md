e7bfcd42d072a7b65d6d24752b29faa531e722c0
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2022-11-08 15:10:06 +0000

    Script to help with building locally, and readme comment.
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Wrapping
-This repository wraps Galasa dependencies that are not in an OSGI bundle into an OSGI bundles which can be run in Galasa. 
+This repository wraps Galasa dependencies that are not in an OSGI bundle into a OSGI bundles which can be run in Galasa. 
 
 ## Documentation
 
@@ -21,4 +21,17 @@ Take a look at the [contribution guidelines](https://github.com/galasa-dev/proje
 
 This code is under the [Eclipse Public License 2.0](https://github.com/galasa-dev/maven/blob/main/LICENSE).
 
-hello things
+## Building locally
+To build locally, you will need to generate a GPG key to sign the articacts with, so that you can prove 
+the source of the artifacts are from you. The Private part of the GPG public-private keypair is used to 
+do the signing. The GPG key is typically installed into your keyring/certificate store, and a passphrase/alias is 
+used to address the private key. 
+When we build artifacts, we sign them prior to deploying them to any external repository, so others can 
+tell that the artifacts haven't been tampered-with, and that the source of the artifacts is genuine.
+This is known as non-repudiation.
+
+To build locally, use the `./build-locally.sh --help` command to see what other parameters are needed.
+
+See the [documentation for the maven gpg plugin](https://maven.apache.org/plugins/maven-gpg-plugin/usage.html)
+for information on other ways of specifying the GPG key to the build process.
+
---
+++ build-locally.sh
@@ -0,0 +1,164 @@
+#! /usr/bin/env bash 
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Objectives: Build this repository code locally.
+# 
+#-----------------------------------------------------------------------------------------                   
+
+# Where is this script executing from ?
+BASEDIR=$(dirname "$0");pushd $BASEDIR 2>&1 >> /dev/null ;BASEDIR=$(pwd);popd 2>&1 >> /dev/null
+# echo "Running from directory ${BASEDIR}"
+export ORIGINAL_DIR=$(pwd)
+# cd "${BASEDIR}"
+
+cd "${BASEDIR}/.."
+WORKSPACE_DIR=$(pwd)
+
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Set Colors
+#
+#-----------------------------------------------------------------------------------------                   
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+white=$(tput setaf 7)
+tan=$(tput setaf 202)
+blue=$(tput setaf 25)
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Headers and Logging
+#
+#-----------------------------------------------------------------------------------------                   
+underline() { printf "${underline}${bold}%s${reset}\n" "$@"
+}
+h1() { printf "\n${underline}${bold}${blue}%s${reset}\n" "$@"
+}
+h2() { printf "\n${underline}${bold}${white}%s${reset}\n" "$@"
+}
+debug() { printf "${white}%s${reset}\n" "$@"
+}
+info() { printf "${white}➜ %s${reset}\n" "$@"
+}
+success() { printf "${green}✔ %s${reset}\n" "$@"
+}
+error() { printf "${red}✖ %s${reset}\n" "$@"
+}
+warn() { printf "${tan}➜ %s${reset}\n" "$@"
+}
+bold() { printf "${bold}%s${reset}\n" "$@"
+}
+note() { printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@"
+}
+
+#-----------------------------------------------------------------------------------------                   
+# Functions
+#-----------------------------------------------------------------------------------------                   
+function usage {
+    info "Syntax: build-locally.sh [OPTIONS]"
+    cat << EOF
+Options are:
+-p | --passphrase - Optional. A GPG passphrase to use to sign the artifacts created.
+    This can be the same GPG passphrase as you use to sign github commits if you wish, 
+    provided that the artifacts are never released or deployed without being re-built
+    with the official verification GPG key.
+    The passphrase will default to the value of the GPG_PASSPHRASE environment variable
+    if available, otherwise the command will fail until some sort of passphrase is 
+    supplied.
+-h | --help - See this help.
+
+Environment variables
+LOGS_DIR - Optional. Where logs are placed. Defaults to creating a temporary directory.
+GPG_PASSPHRASE - The passphrase. Over-ridden if the --passphrase parameter is used.
+EOF
+}
+
+
+#-----------------------------------------------------------------------------------------                   
+# Process parameters
+#-----------------------------------------------------------------------------------------                   
+build_type=""
+
+gpg_passphrase=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --passphrase )
+                                shift
+                                export pg_passphrase="$1"
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     error "Unexpected argument $1"
+                                usage
+                                exit 1
+    esac
+    shift
+done
+
+if [[ "${gpg_passphrase}" == "" ]]; then
+
+    if [[ "${GPG_PASSPHRASE}" != "" ]]; then
+        info "Environment variable GPG_PASSPHRASE is being used to sign the built artifacts."
+        export gpg_passphrase="${GPG_PASSPHRASE}"
+    else
+        error "Need to use the --passphrase parameter to supply a GPG key to be used for signing artifacts. Or supply a value with the GPG_PASSPHRASE environment variable."
+        usage
+        exit 1  
+    fi
+fi
+
+
+#-----------------------------------------------------------------------------------------                   
+# Main logic.
+#-----------------------------------------------------------------------------------------                   
+
+source_dir="."
+
+project=$(basename ${BASEDIR})
+h1 "Building ${project}"
+
+# Create a temporary dir.
+# Note: This bash 'spell' works in OSX and Linux.
+if [[ -z ${LOGS_DIR} ]]; then
+    export LOGS_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t "galasa-logs")
+    info "Logs are stored in the ${LOGS_DIR} folder."
+    info "Over-ride this setting using the LOGS_DIR environment variable."
+else
+    mkdir -p ${LOGS_DIR} 2>&1 > /dev/null # Don't show output. We don't care if it already existed.
+    info "Logs are stored in the ${LOGS_DIR} folder."
+    info "Over-ridden by caller using the LOGS_DIR variable."
+fi
+
+info "Using source code at ${source_dir}"
+cd ${BASEDIR}/${source_dir}
+
+log_file=${LOGS_DIR}/${project}.txt
+info "Log will be placed at ${log_file}"
+
+h2 "Building..."
+cat << EOF
+
+Using this command:
+
+mvn clean install -Dgpg.passphrase=\$GPG_PASSPHRASE
+2>&1 >> ${log_file}
+
+EOF
+
+mvn clean install -Dgpg.passphrase=${gpg_passphrase} \
+2>&1 >> ${log_file}
+rc=$? ; if [[ "${rc}" != "0" ]]; then 
+    error "Failed to build ${project}" 
+    info "See log file at ${log_file}"
+    exit 1
+fi
+success "Built OK"
+
+success "Project ${project} built - OK - log is at ${log_file}"

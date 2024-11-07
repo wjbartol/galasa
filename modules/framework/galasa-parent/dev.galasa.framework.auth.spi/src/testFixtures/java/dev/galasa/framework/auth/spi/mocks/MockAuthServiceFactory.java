4694c35835632a87e205b191adfca258cdac1d87
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-11-07 09:41:34 +0000

    chore: Add missing copyright
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- modules/framework/galasa-parent/dev.galasa.framework.auth.spi/src/testFixtures/java/dev/galasa/framework/auth/spi/mocks/MockAuthServiceFactory.java
+++ modules/framework/galasa-parent/dev.galasa.framework.auth.spi/src/testFixtures/java/dev/galasa/framework/auth/spi/mocks/MockAuthServiceFactory.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package dev.galasa.framework.auth.spi.mocks;
 
 import javax.servlet.ServletException;
@@ -17,5 +22,4 @@ public class MockAuthServiceFactory implements IAuthServiceFactory {
     public IAuthService getAuthService() throws ServletException {
         return authService;
     }
-    
 }

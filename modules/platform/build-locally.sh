9acac0250da632f6e26797009f56f510581d6031
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-11-13 17:45:31 +0000

    build locally also builds the platform now.
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- modules/platform/build-locally.sh
+++ modules/platform/build-locally.sh
@@ -1,3 +1,81 @@
 #! /usr/bin/env bash
-cd dev.galasa.platform
-gradle build check publishToMavenLocal
+
+#
+# Copyright contributors to the Galasa project
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+#-----------------------------------------------------------------------------------------
+#
+# Objectives: Build this repository code locally.
+#
+
+# Where is this script executing from ?
+BASEDIR=$(dirname "$0");pushd $BASEDIR 2>&1 >> /dev/null ;BASEDIR=$(pwd);popd 2>&1 >> /dev/null
+# echo "Running from directory ${BASEDIR}"
+export ORIGINAL_DIR=$(pwd)
+cd "${BASEDIR}"
+
+#-----------------------------------------------------------------------------------------
+#
+# Set Colors
+#
+#-----------------------------------------------------------------------------------------
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+white=$(tput setaf 7)
+tan=$(tput setaf 202)
+blue=$(tput setaf 25)
+
+#-----------------------------------------------------------------------------------------
+#
+# Headers and Logging
+#
+#-----------------------------------------------------------------------------------------
+underline() { printf "${underline}${bold}%s${reset}\n" "$@" ;}
+h1() { printf "\n${underline}${bold}${blue}%s${reset}\n" "$@" ;}
+h2() { printf "\n${underline}${bold}${white}%s${reset}\n" "$@" ;}
+debug() { printf "${white}%s${reset}\n" "$@" ;}
+info() { printf "${white}➜ %s${reset}\n" "$@" ;}
+success() { printf "${green}✔ %s${reset}\n" "$@" ;}
+error() { printf "${red}✖ %s${reset}\n" "$@" ;}
+warn() { printf "${tan}➜ %s${reset}\n" "$@" ;}
+bold() { printf "${bold}%s${reset}\n" "$@" ;}
+note() { printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@" ;}
+
+#-------------------------------------------------------------
+function check_exit_code () {
+    # This function takes 2 parameters in the form:
+    # $1 an integer value of the returned exit code
+    # $2 an error message to display if $1 is not equal to 0
+    if [[ "$1" != "0" ]]; then 
+        error "$2" 
+        exit 1  
+    fi
+}
+
+
+h1 "Building the platform module"
+
+function build_platform() {
+    h2 "Building the platformn source using gradle."
+    cd $BASEDIR/dev.galasa.platform
+    rc=$?
+    check_exit_code $rc "Failed to cd to the platform source folder"
+    gradle build check publishToMavenLocal
+    rc=$?
+    check_exit_code $rc "Failed to build the plaform module source"
+    success "OK"
+}
+
+function clean_platform() {
+    h2 "Cleaning the exisitng built platform in the ~/.m2 repository"
+    rm -fr ~/.m2/repository/dev/galasa/dev.galasa.platform
+}
+
+clean_platform
+build_platform
